20b32231ebef55398748f9d131872f8d4ef7f790
aloizio.oliveira <aloizio.oliveira@totvs.com.br>
2026-08-10 19:34:51 -0300

    A

--- code-samples/1-Kustomization-file/wordpress/kustomize/Configmaps/scripts/start.sh
+++ code-samples/1-Kustomization-file/wordpress/kustomize/Configmaps/scripts/start.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-# Expose k3d node port to localhost for testing (if you are using k3d on a mac)
-#  k3d cluster create kustomize-test --servers=2 --agents=2 --port="30000-30010:30000-30010@loadbalancer"
 
 # Create a namespace for the lecture
 kubectl create ns lec-12

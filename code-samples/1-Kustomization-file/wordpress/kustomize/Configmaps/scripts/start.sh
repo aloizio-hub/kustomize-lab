3eb8ee7439ee2eeab6d59215b8c02e7e8f94749b
aloizio.oliveira <aloizio.oliveira@totvs.com.br>
2026-08-10 22:13:54 -0300

    asasa

--- code-samples/1-Kustomization-file/wordpress/kustomize/Configmaps/scripts/start.sh
+++ code-samples/1-Kustomization-file/wordpress/kustomize/Configmaps/scripts/start.sh
@@ -2,13 +2,13 @@
 
 
 # Create a namespace for the lecture
-kubectl create ns lec-12
+k create ns techtalk-cm
 
-# Set the current namespace to lec-12
-kubectl config set-context --current --namespace=lec-12
+# Set the current namespace to techtalk-cm
+k config set-context --current --namespace=techtalk-cm
 
 # Apply the kustomization to the cluster
-kubectl apply -k .
+k apply -k .
 
 # Get the postgres database ip address
-kubectl get svc/lec-12-mysql --output="jsonpath={.spec.clusterIP}"
+k get svc/lec-12-mysql --output="jsonpath={.spec.clusterIP}"
